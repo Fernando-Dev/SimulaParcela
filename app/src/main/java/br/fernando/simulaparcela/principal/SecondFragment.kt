79764b379769ddf04ebca4363c4e66494feefff9
Fernando Nascimento <fernando_dev@outlook.com.br>
2021-03-15 21:14:33 -0300

    alterando funcao didtribuicao parcela

--- app/src/main/java/br/fernando/simulaparcela/principal/SecondFragment.kt
+++ app/src/main/java/br/fernando/simulaparcela/principal/SecondFragment.kt
@@ -1,5 +1,7 @@
 package br.fernando.simulaparcela.principal
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
@@ -10,6 +12,7 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import br.fernando.simulaparcela.R
@@ -260,16 +263,34 @@ class SecondFragment : Fragment() {
             edtQtdeParcela.setText("")
         }
 
+
         btnCalcularDistribuicao.setOnClickListener {
             esconderTeclado(secondView)
 
             containerResultado.removeAllViews()
 
-            val txtValorA = transformaValor(edtValorA.text.toString())
-            val txtValorB = transformaValor(edtValorB.text.toString())
-            val txtValorC = transformaValor(edtValorC.text.toString())
-            val txtValorD = transformaValor(edtValorD.text.toString())
-            val txtValorParcela = transformaValor(edtValorParcela.text.toString())
+            var txtValorA = 0.0
+            var txtValorB = 0.0
+            var txtValorC = 0.0
+            var txtValorD = 0.0
+            var txtValorParcela = 0.0
+
+            if (edtValorA.text.toString().isNotBlank()) {
+                txtValorA = transformaValor(edtValorA.text.toString())
+            }
+            if (edtValorB.text.toString().isNotBlank()) {
+                txtValorB = transformaValor(edtValorB.text.toString())
+            }
+            if (edtValorC.text.toString().isNotBlank()) {
+                txtValorC = transformaValor(edtValorC.text.toString())
+            }
+            if (edtValorD.text.toString().isNotBlank()) {
+                txtValorD = transformaValor(edtValorD.text.toString())
+            }
+            if (edtValorParcela.text.toString().isNotBlank()) {
+                txtValorParcela = transformaValor(edtValorParcela.text.toString())
+            }
+
 
             if (txtNomeA.text.toString().isBlank() || edtValorA.text.toString().isBlank() || (txtValorA.equals(0.0))) {
                 Snackbar.make(layout_principal, "NOME A OU VALOR A ESTÁ VAZIO", Snackbar.LENGTH_LONG).show()
@@ -290,8 +311,119 @@ class SecondFragment : Fragment() {
                 Snackbar.make(layout_principal, "NOME D OU VALOR D ESTÁ VAZIO", Snackbar.LENGTH_LONG).show()
             } else if (edtValorParcela.text.toString().isBlank() || (txtValorParcela.equals(0.0))) {
                 Snackbar.make(layout_principal, "VALOR DA PARCELA ESTÁ VAZIO", Snackbar.LENGTH_LONG).show()
+            }else if(edtQtdeParcela.text.toString().isBlank()){
+                Snackbar.make(layout_principal, "QUANTIDADE DE PARCELAS ESTÁ VAZIA", Snackbar.LENGTH_LONG).show()
+            } else {
 
-            }else{
+                val valorA = transformaValor(edtValorA.text.toString())
+                val valorB = transformaValor(edtValorB.text.toString())
+                val valorC = transformaValor(edtValorC.text.toString())
+                val valorD = transformaValor(edtValorD.text.toString())
+
+                val valorParcela = transformaValor(edtValorParcela.text.toString())
+                val somaValorEmprestimo = calculoSomaEmprestimo(valorA, valorB, valorC, valorD)
+
+                val resultadoA = calculaParcela(valorParcela, somaValorEmprestimo, valorA)
+                val resultadoB = calculaParcela(valorParcela, somaValorEmprestimo, valorB)
+                val resultadoC = calculaParcela(valorParcela, somaValorEmprestimo, valorC)
+                val resultadoD = calculaParcela(valorParcela, somaValorEmprestimo, valorD)
+
+                val infla = getContext()?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+                val linhaResultadoDist =
+                    infla.inflate(R.layout.item_distribuicao_resultado, containerResultado, false)
+
+                val nomeDistResultadoA = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_nome_a)
+                val nomeDistResultadoB = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_nome_b)
+                val nomeDistResultadoC = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_nome_c)
+                val nomeDistResultadoD = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_nome_d)
+
+                val valorDistResultadoA = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_a)
+                val valorDistResultadoB = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_b)
+                val valorDistResultadoC = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_c)
+                val valorDistResultadoD = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_d)
+
+                val parcelaDistResultadoA = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_parcela_a)
+                val parcelaDistResultadoB = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_parcela_b)
+                val parcelaDistResultadoC = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_parcela_c)
+                val parcelaDistResultadoD = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_parcela_d)
+
+                val qtdeParcelas = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_qtde_parcelas)
+
+                val valorTotalParcela = linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_total_parcela)
+
+                val valorTotalEmprestimo =
+                    linhaResultadoDist.findViewById<TextView>(R.id.txt_res_dist_valor_total_emprestimo)
+
+                val btnCopiarTextDist = linhaResultadoDist.findViewById<Button>(R.id.btn_copiar_texto_distribuicao)
+
+                nomeDistResultadoA.setText(txtNomeA.text.toString())
+                nomeDistResultadoB.setText(txtNomeB.text.toString())
+                nomeDistResultadoC.setText(txtNomeC.text.toString())
+                nomeDistResultadoD.setText(txtNomeD.text.toString())
+
+                valorDistResultadoA.setText(arredondar(valorA))
+                valorDistResultadoB.setText(arredondar(valorB))
+                valorDistResultadoC.setText(arredondar(valorC))
+                valorDistResultadoD.setText(arredondar(valorD))
+
+                parcelaDistResultadoA.setText(arredondar(resultadoA))
+                parcelaDistResultadoB.setText(arredondar(resultadoB))
+                parcelaDistResultadoC.setText(arredondar(resultadoC))
+                parcelaDistResultadoD.setText(arredondar(resultadoD))
+
+                qtdeParcelas.setText(edtQtdeParcela.text.toString())
+
+                valorTotalParcela.setText(
+                    arredondar(
+                        calculoSomaEmprestimo(
+                            resultadoA,
+                            resultadoB,
+                            resultadoC,
+                            resultadoD
+                        )
+                    )
+                )
+
+                valorTotalEmprestimo.setText(arredondar(somaValorEmprestimo))
+
+                btnCopiarTextDist.setOnClickListener {
+                    esconderTeclado(linhaResultadoDist)
+                    //                                    pegando o clipboard system service
+                    val clipBoardManager =
+                        getContext()?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val textoCopiado =
+                        "Cliente: " + "${nomeDistResultadoA.text}" +" -> "+
+                                "Valor: " + "${valorDistResultadoA.text}" +" -> "+
+                                "Parcela: " + "${parcelaDistResultadoA.text}" + "\n" +
+                                "---------------------------------------"+"\n"+
+                                "Cliente: " + "${nomeDistResultadoB.text}" +" -> "+
+                                "Valor: " + "${valorDistResultadoB.text}" +" -> "+
+                                "Parcela: " + "${parcelaDistResultadoB.text}" + "\n" +
+                                "---------------------------------------"+"\n"+
+                                "Cliente: " + "${nomeDistResultadoC.text}" +" -> "+
+                                "Valor: " + "${valorDistResultadoC.text}" +" -> "+
+                                "Parcela: " + "${parcelaDistResultadoC.text}" + "\n" +
+                                "---------------------------------------"+"\n"+
+                                "Cliente: " + "${nomeDistResultadoD.text}" +" -> "+
+                                "Valor: " + "${valorDistResultadoD.text}" +" -> "+
+                                "Parcela: " + "${parcelaDistResultadoD.text}" + "\n" +
+                                "---------------------------------------"+"\n"+
+                                "Qtde Parcelas: " + "${qtdeParcelas.text}" + "\n" +
+                                "Valor da Parcela: " + "${valorTotalParcela.text}" + "\n" +
+                                "Valor Total: " + "${valorTotalEmprestimo.text}"
+//                                    configurando a string para o recebimento das informacoes
+//                                    passando a string para o clipdata
+                    val clip = ClipData.newPlainText("RESULTADO", textoCopiado)
+//                                    setando a string para o clipboardmanager
+                    clipBoardManager.setPrimaryClip(clip)
+//                                    toast para avisar o usuario
+                    Snackbar.make(layout_principal, "TEXTO COPIADO!", Snackbar.LENGTH_LONG).show()
+
+
+                }
+
+                containerResultado.addView(linhaResultadoDist)
 
             }
 
@@ -320,6 +452,21 @@ class SecondFragment : Fragment() {
         val v = DecimalFormat("R$ #,##0.00").format(valor)
         return v
     }
+
+    fun calculoSomaEmprestimo(v1: Double, v2: Double, v3: Double, v4: Double): Double {
+        val soma = v1 + v2 + v3 + v4
+        return soma
+    }
+
+    fun calculaParcela(valorParcela: Double, somaEmprestimo: Double, valorEmprestimo: Double): Double {
+
+        val resultadoDivisao = valorParcela / somaEmprestimo
+
+        val resultado = valorEmprestimo * resultadoDivisao
+
+        return resultado
+    }
+
 
     fun transformaValor(valor: String): Double {
 //        PEGANDO O LOCAL
